48ef395352b05237f22b037a60b1bba68a31b5ca
xweetx <xweet_xilenze@yahoo.com>
2023-06-30 01:43:47 -0600

    changes

--- express_server.js
+++ express_server.js
@@ -1,33 +1,25 @@
-const express = require(`express`);
+const express = require("express");
 const app = express();
+const PORT = 8080;
 
-const PORT = 8080; // default port 8080
-
-app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
+app.set("view engine", "ejs");
 
 const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
-/*Routes*/
+/* Routes */
 
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 
-//gets page of URL index
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
 });
-
-app.post("/urls", (req, res) => {
-  console.log(req.body); // Log the POST request body to the console
-  res.send("Ok"); // Respond with 'Ok' (we will replace this)
-});
-
 
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
@@ -36,6 +28,17 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
+});
+
+app.post("/login", (req, res) => {
+  const { username } = req.body;
+  res.cookie("username", username);
+  res.redirect("/urls");
+});
+
+app.post("/urls", (req, res) => {
+  console.log(req.body);
+  res.send("Ok");
 });
 
 app.get("/u/:id", (req, res) => {
@@ -48,10 +51,6 @@ app.get("/u/:id", (req, res) => {
   }
 });
 
-app.listen(PORT, () => {
-  console.log(`Example app listening on port ${PORT}!`);
-});
-
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
@@ -60,3 +59,6 @@ app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
 
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
