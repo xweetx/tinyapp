f7a76dfa0fb6d1cd7d4a80e0d1f91e8b6f31f05f
xweetx <xweet_xilenze@yahoo.com>
2023-06-30 01:37:42 -0600

    changes

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-const express = require("express");
+const express = require(`express`);
 const app = express();
 const PORT = 8080; // default port 8080
 
@@ -13,43 +13,11 @@ const urlDatabase = {
 
 /*Routes*/
 
-// redirects to /urls when logged in, sent to /login if not
 app.get("/", (req, res) => {
-  if (req.session.user_ID) {
-    res.redirect("/urls");
-  } else {
-    res.redirect("/login");
-  }
+  res.send("Hello!");
 });
 
-//urls index page (GET) - user's urls when logged in
-app.get('/urls', (req, res) => {
-  const userID = req.session.userID;
-  const userUrls = urlsForUser(userID, urlDatabase);
-  const templateVars = { urls: userUrls, user: users[userID] };
-  
-  if (!userID) {
-    res.statusCode = 401;
-  }
-  
-  res.render('urls_index', templateVars);
-});
-
-//creates new url (POST) - adds new url to database and redirects to short url page
-app.post('/urls', (req, res) => {
-  if (req.session.userID) {
-    const shortURL = generateRandomString();
-    urlDatabase[shortURL] = {
-      longURL: req.body.longURL,
-      userID: req.session.userID
-    };
-    res.redirect(`/urls/${shortURL}`);
-  } else {
-    const errorMessage = 'You must be logged in to do that.';
-    res.status(401).render('urls_error', {user: users[req.session.userID], errorMessage});
-  }
-});
-
+//gets page of URL index
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -61,7 +29,6 @@ app.post("/urls", (req, res) => {
 });
 
 
-// new url create page (GET)
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
@@ -69,12 +36,6 @@ app.get("/urls/new", (req, res) => {
 app.get("/urls/:id", (req, res) => {
   const templateVars = { id: req.params.id, longURL: urlDatabase[req.params.id] };
   res.render("urls_show", templateVars);
-});
-
-app.post("/login", (req, res) => {
-  const { username } = req.body;
-  res.cookie("username", username);
-  res.redirect("/urls");
 });
 
 app.get("/u/:id", (req, res) => {
