d5b7acac6cb253a9a385f0ef2f469eb8354477bc
xweetx <xweet_xilenze@yahoo.com>
2023-06-29 23:53:34 -0600

    changes

--- express_server.js
+++ express_server.js
@@ -13,11 +13,43 @@ const urlDatabase = {
 
 /*Routes*/
 
+// redirects to /urls when logged in, sent to /login if not
 app.get("/", (req, res) => {
-  res.send("Hello!");
+  if (req.session.user_ID) {
+    res.redirect("/urls");
+  } else {
+    res.redirect("/login");
+  }
 });
 
-//gets page of URL index
+//urls index page (GET) - user's urls when logged in
+app.get('/urls', (req, res) => {
+  const userID = req.session.userID;
+  const userUrls = urlsForUser(userID, urlDatabase);
+  const templateVars = { urls: userUrls, user: users[userID] };
+  
+  if (!userID) {
+    res.statusCode = 401;
+  }
+  
+  res.render('urls_index', templateVars);
+});
+
+//creates new url (POST) - adds new url to database and redirects to short url page
+app.post('/urls', (req, res) => {
+  if (req.session.userID) {
+    const shortURL = generateRandomString();
+    urlDatabase[shortURL] = {
+      longURL: req.body.longURL,
+      userID: req.session.userID
+    };
+    res.redirect(`/urls/${shortURL}`);
+  } else {
+    const errorMessage = 'You must be logged in to do that.';
+    res.status(401).render('urls_error', {user: users[req.session.userID], errorMessage});
+  }
+});
+
 app.get("/urls", (req, res) => {
   const templateVars = { urls: urlDatabase };
   res.render("urls_index", templateVars);
@@ -29,6 +61,7 @@ app.post("/urls", (req, res) => {
 });
 
 
+// new url create page (GET)
 app.get("/urls/new", (req, res) => {
   res.render("urls_new");
 });
